775271bb4642113b66f5c4a0939260ad3a96b39a
Dennis Akagha <d.akagha20@gmail.com>
2024-03-24 22:04:03 +0100

    learn about serializer.serializermethodfileds

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
+from .views import Blog_list_APIView
 
 
 urlpatterns = [
-   path("", views.blog_list, name='blog-list'),
-   path("updated/<int:id>/", views.blog_update, name='blog-update')
+   # path("", views.blog_list, name='blog-list'),
+   # path("updated/<int:id>/", views.blog_update, name='blog-update'),
+   path("", Blog_list_APIView.as_view(), name='blog-list'),
 ]
